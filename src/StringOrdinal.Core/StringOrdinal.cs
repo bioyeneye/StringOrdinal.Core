75a10335fc1d69b206a0cf2afc3e7a2d602680bd
Bolaji Oyeneye <bioyeneye@gmail.com>
2020-08-13 20:05:34 +0100

    added sample code

--- src/StringOrdinal.Core/StringOrdinal.cs
+++ src/StringOrdinal.Core/StringOrdinal.cs
@@ -2,6 +2,29 @@ namespace StringOrdinal.Core
 {
     public class StringOrdinal
     {
-        
+        public static string ConstructOrdinalToNumber(int num)
+        {
+            if( num <= 0 ) return num.ToString();
+
+            switch(num % 100)
+            {
+                case 11:
+                case 12:
+                case 13:
+                    return num + "ᵗʰ";
+            }
+
+            switch(num % 10)
+            {
+                case 1:
+                    return num + "ˢᵗ";
+                case 2:
+                    return num + "ⁿᵈ";
+                case 3:
+                    return num + "ʳᵈ";
+                default:
+                    return num + "ᵗʰ";
+            }
+        }
     }
 }
---
+++ src/StringOrdinal.SampleConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringOrdinal.SampleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
